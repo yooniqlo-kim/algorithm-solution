126df4ac64d5df11022a013347ed94d2b88179a4
yooniqlo <yoon733@naver.com>
2024-08-03 02:15:04 +0900

    [level 0] Title: rny_string, Time: 0.23 ms, Memory: 74 MB -BaekjoonHub

--- Java/프로그래머스/0/181863. rny＿string/rny＿string.java
+++ Java/프로그래머스/0/181863. rny＿string/rny＿string.java
@@ -2,21 +2,12 @@ import java.util.*;
 import java.io.*;
 class Solution {
     public String solution(String rny_string) {
-		List<Character> list = new ArrayList<>();
+		StringBuilder sb = new StringBuilder();
 		
-		for(int i=0; i< rny_string.length(); i++) {
-			list.add(rny_string.charAt(i));
-		}
-		
-		for(int i=0; i<list.size(); i++) {
-			if(list.get(i).equals('m')) {
-				list.set(i, 'r');
-				list.add(i+1, 'n');
-			}
-		}
-        StringBuffer sb = new StringBuffer();
-		for(char c : list)
-			sb.append(c);
+		for(char c : rny_string.toCharArray())
+			sb.append(c == 'm' ? "rn" : c);
+        
         return sb.toString();
+        // return rny_string.replaceAll("m", "rn");
     }
 }
