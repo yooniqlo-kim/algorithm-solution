6ce4a6338ac8822c7d1f4e6be197ee5bc8564ca6
yooniqlo <yoon733@naver.com>
2024-08-09 01:01:28 +0900

    [Bronze II] Title: 막대기, Time: 252 ms, Memory: 25064 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.io.*;
+
+public class Main { 
+	public static void main(String[] args) throws Exception{
+		//System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		int N = Integer.parseInt(br.readLine().trim());
+		int max = 0, cnt=1;
+		
+		for(int i=0; i<N; i++) {
+			stack.push(Integer.parseInt(br.readLine().trim()));
+		}
+
+		int longStick;
+		int stick = stack.pop();
+		while(!stack.isEmpty()) {
+			longStick = stack.pop();
+			if(longStick > stick) {
+				stick = longStick;
+				cnt++;
+			}
+			
+		}
+		
+		System.out.println(cnt);
+		
+
+	}// end main
+
+} //end class
+
