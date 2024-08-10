61292452e6673f2128393894886e04d74bb63729
yooniqlo <yoon733@naver.com>
2024-08-11 00:05:00 +0900

    [Silver V] Title: 종이자르기, Time: 100 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.java
+++ 백준/Silver/2628. 종이자르기/종이자르기.java
@@ -10,42 +10,47 @@ public class Main {
 		st = new StringTokenizer(br.readLine().trim());
 		int width = Integer.parseInt(st.nextToken()); // 종이의 가로 길이
 		int height = Integer.parseInt(st.nextToken()); // 종이의 세로 길이
-		List<Integer> targetRowList = new ArrayList<Integer>();
-		List<Integer> targetColList = new ArrayList<Integer>();
+		
+		// 가로줄과 세로줄 점선 위치를 저장하는 리스트
+		List<Integer> targetRowList = new ArrayList<Integer>(); //가로줄의 점선위치
+		List<Integer> targetColList = new ArrayList<Integer>(); //세로줄의 점선 위치
 		
 		int N = Integer.parseInt(br.readLine().trim()); // 잘라야 하는 점선의 개수
 		
 		for(int i=0; i<N; i++) {
 			st = new StringTokenizer(br.readLine().trim());
-			int rowColCheck = Integer.parseInt(st.nextToken()); // 0이면 가로줄, 1이면 세로줄
+			int rowColCheck = Integer.parseInt(st.nextToken());
 			
-			if (rowColCheck == 0) {
+			if (rowColCheck == 0) {  // 0이면 가로줄
 				targetRowList.add(Integer.parseInt(st.nextToken()));
-			} else if (rowColCheck == 1) {
+			} else if (rowColCheck == 1) { // 1이면 세로줄
 				targetColList.add(Integer.parseInt(st.nextToken()));
 			}		
 		}
-		targetRowList.add(height);
+		
+		targetRowList.add(height); //종이의 세로 길이 추가
 		targetRowList.add(0);
-		targetColList.add(width);
+		targetColList.add(width); //종이의 가로 길이 추가
 		targetColList.add(0);
 		
-		Collections.sort(targetRowList);
+		Collections.sort(targetRowList); //오름차순 정렬
 		Collections.sort(targetColList);
 		
 		int maxRowLen = 0, maxColLen = 0;
 		
+		// 가로점선 잘랐을 때, 최대 세로 길이 계산
 		for(int i=1; i<targetRowList.size(); i++) {
 			int tmp = targetRowList.get(i)-targetRowList.get(i-1);
 			maxColLen = Math.max(maxColLen, tmp);
 		}
 		
+		// 세로점선 잘랐을 때, 최대 가로 길이 계산
 		for(int i=1; i<targetColList.size(); i++) {
 			int tmp = targetColList.get(i) - targetColList.get(i-1);
 			maxRowLen = Math.max(maxRowLen, tmp);
 		}
 		
-		int result = maxRowLen * maxColLen;
+		int result = maxRowLen * maxColLen; //최대 넓이 계산
 		
 		System.out.println(result);
 		
